7023738f77ba20a2399a477083db51597a730e65
Joe Rivera <joe.rivera@cyberpcangel.com>
2026-09-03 17:14:02 -0500

    docs(licence): add the project licence banner to files that had none
    44 sources carried no licence header at all. Each now gets the same UT
    Austin LGPL-2.1 banner the rest of the tree uses, with the copyright year
    taken from when the file first appeared and the author line taken from a
    sibling in the same module where the tree already records one.
    
    Files deliberately left alone, because they are not this project's work and
    stamping a UT banner on them would be a false claim:
    
      src/f3dock-loop-closure/sturm.h   Graphics Gems (Hook & McAree), via
                                        Chaok Seok; provenance comment, no grant
      inc/PG-range/dsexceptions.h       nine lines, provenance unknown
      inc/libmol/newgetline.h           libmol; Boston University notice applies
      src/libmol/newgetline.cpp         to the whole tree
      src/libmol/test.cpp
      src/libmol/testHbond.cpp
    
    inc/PG-range/cuckoo.h and src/PG-range/cuckoo.cc already carry a permissive
    BRICS grant and needed nothing. All six abstentions are recorded in
    THIRD-PARTY.md.

--- src/f3dock-flex/FlexSampler.cpp
+++ src/f3dock-flex/FlexSampler.cpp
@@ -1,3 +1,24 @@
+/*
+  Copyright 2026 The University of Texas at Austin
+
+        Advisor: Chandrajit Bajaj <bajaj@cs.utexas.edu>
+
+  This file is part of F2Dock.
+
+  F2Dock is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License version 2.1 as published by the Free Software Foundation.
+
+  F2Dock is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+*/
+
 #include "f3dock/flex/FlexSampler.h"
 
 #include "f3dock/util/StringUtil.h"
